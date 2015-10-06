493f1b9c4b174fe3540554dc3e46020e674bdcb0
Ron <2012042076@ust-ics.mygbiz.com>
2015-10-06 21:51:56 +0800

    Initial Frontend
    Made with AngularJS, Bootstrap 3, HTML5, CSS3, and JQuery

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
 var careerApp = angular.module('careerApp', ['ui.router']);
 
-careerApp.config(function($stateProvider, $urlRouterProvider) {
+careerApp.config(function($stateProvider, $urlRouterProvider, $provide) {
     
+	//Stops auto scrolling when navigating
+	$provide.decorator('$uiViewScroll', function ($delegate) {
+		return function (uiViewElement) {
+			var top = uiViewElement.getBoundingClientRect().top;
+			// window.scrollTo(0, (top - 30)); //alternative
+		}; 
+	});
+
+	//Default directory is Home
     $urlRouterProvider.otherwise('/home');
     
+	//Directories to choose
     $stateProvider
         
         // HOME 
